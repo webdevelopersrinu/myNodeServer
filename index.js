9685d8f348bab343107cbc547e2910d5388f47e2
srinu <webdeveloper.srinu9@gmail.com>
2024-08-31 12:11:39 +0530

    create get route for products data

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
-
+const { productsList } = require("./productsList");
 const app = express();
+
+const products = productsList;
 
 app.get("/", (req, res) => {
   res.status(200).send("welcome to my web this is srinu desetti");
+});
+
+app.get("/products", (req, res) => {
+  try {
+    res.status(200).json({
+      status: "success",
+      data: products,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "Fial",
+      message: err.message,
+    });
+  }
 });
 
 const PORT = process.env.PORT || 3890;
